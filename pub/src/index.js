cf56245659ff67cc4d886ce719f8ec29c392ad2a
Sir Robert Burbridge <sirrobert@gmail.com>
2020-07-26 22:34:48 -0400

    Basic jsdoc docs.

--- pub/src/index.js
+++ pub/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from '@bit/thefarce.repo-docs.app';
 import * as serviceWorker from './serviceWorker';
+import doclets from './jsdoc-data.json';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App doclets={doclets}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/**
+ * A seed function for the repo template.
+ *
+ * @name helloWorld
+ * @returns {undefined} 
+ */
 function helloWorld () {
   console.log("Hello, World!");
 }
